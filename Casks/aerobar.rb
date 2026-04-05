57131797ccd293cd00cd733db75ccb3bdc911753
Bradley Mattison <101834069+Brad-M2K@users.noreply.github.com>
2026-04-05 22:49:48 +0100

    Add AeroBar cask for version 0.5.1

---
+++ Casks/aerobar.rb
@@ -0,0 +1,9 @@
+cask "aerobar" do
+  version "0.5.1"
+  sha256 "PASTE_SHA"
+
+  url "https://github.com/Brad-M2K/barik-2.0/releases/download/v0.5.1/Barik.zip
+  name "AeroBar"
+
+  app "Barik.app"
+end
