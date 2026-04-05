82f13334550aece5da6642c6e08c86968b3bb772
Bradley Mattison <101834069+Brad-M2K@users.noreply.github.com>
2026-04-05 22:54:51 +0100

    Update sha256 for AeroBar cask

--- Casks/aerobar.rb
+++ Casks/aerobar.rb
@@ -1,7 +1,6 @@
 cask "aerobar" do
   version "0.5.1"
-  sha256 "PASTE_SHA"
-
+  sha256 "2c1a03007aa001af62f03cf3794dc4f8e43dc47d94fd3cb7683043eede11c7b9"
   url "https://github.com/Brad-M2K/barik-2.0/releases/download/v0.5.1/Barik.zip
   name "AeroBar"
 
